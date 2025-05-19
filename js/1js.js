00d85b060f281930bca1f2720fab2566e05b1d40
manju <manjumc071@gmail.com>
2025-05-19 20:27:04 +0530

    file add

---
+++ js/1js.js
@@ -0,0 +1,117 @@
+// var fullname = "bca project";
+// age = 23;
+// year = 2023;
+// console.log(fullname, age, year);
+
+// var username = "BCA project";
+// var username = "manju";
+
+// console.log(username);
+
+// var username = "BCA project";
+// var username = "manju";
+// var Username = "manju umar"
+
+// console.log(Username);
+
+// let a = 23;
+// age = 24;
+// console.log(age);
+
+
+// let age;
+// console.log(age);
+
+// {
+//     let a = 10;
+//     console.log(a);
+// }
+
+// {
+//     let a = 20;
+//     console.log(a);
+// }
+
+// const a = 3.142;
+// console.log(a);
+
+// const TAX_RATE = 0.18;
+// const basePrice = 100;
+
+// const finalPrice = basePrice + basePrice * TAX_RATE;
+
+// console.log("Final Price with Tax: $" + finalPrice)
+
+// =========Boolean=========
+
+// let isTrue = true;
+// let isFalse = false;
+
+// console.log(isTrue);
+// console.log(isFalse);
+
+// ======null======
+// let myVariable = null;
+// console.log(myVariable);
+
+
+// =======undefined======
+
+// let myVariable;
+// console.log(myVariable);
+
+// ======Symbol=======
+
+// let sym=Symbol("This is a symbol");
+// console.log(sym);
+
+// =====BigInt======
+
+// let myBigInt
+
+// =============================================multi line Comment=========================================//
+
+// =====Arthmetic operators=======//
+
+// let a = 5;
+// let b = 10;
+
+// console.log("a+b=", a + b);
+
+// ====assignment operators======//
+
+// a += b;
+// console.log("a += b =", a);
+// a -= b;
+// console.log("a = b =", a);
+// a *= b;
+// console.log("a *= b =", a);
+// a /= b;
+// console.log("a /= b =", a);
+// a %= b;
+// console.log("a %= b =", a);
+// a **= b;
+// console.log("a **= b =", a);
+
+// =====exponentiation======
+// console.log("a**b=", a ** b);
+
+// ======increment and decrement======//
+
+// console.log("++a =", ++a);
+// console.log("--a=", --a);
+
+// ====comparison operators======//
+
+// console.log("a==b=")
+
+
+// ====logical operators===//
+
+// let c = 5;
+// let d = 10;
+// let e = c + d == 15;
+// let f = c + d != 15;
+// console.log("e&&f =", e && f);
+// console.log("e||f", e || f);
+// console.log("!e=", !e);
---
+++ js/whileloop.js
@@ -0,0 +1,123 @@
+// let count=1;
+
+// while (coun<=5){
+//     console.log("count is:"+count);
+//     count++;
+// }
+
+// ====sum of 1 to 5 in js using for loop=====//
+
+// let sum = 0;
+
+// for (let i = 1; i <= 5; i++) {
+//     sum += i;
+// }
+
+// console.log("Sum from 1 to 5 is:"
+
+// =====do while===//
+
+// let count = 1;
+
+// do {
+//     console.log("count is :" + count);
+//     count++
+// }
+// while (count <= 5);
+
+// let count = 1;
+
+// do {
+//     console.log(count + " " + "manjunath ");
+//     count++
+// }
+// while (count <= 20);
+
+// // ============================for-off loop===========================//
+
+// let str = "manjunath";
+
+// for (let i = 0; i < str.length; i++) {
+//     console.log(str[i]);
+// }
+
+// // // ========================for-in loop======================//
+
+// // let student = {
+// //     name: "bcapeoject",
+// //     age: 20,
+// //     grade: "A",
+// // };
+
+// // for (let key in student) {
+// //     console.log(key);
+// // }
+
+// for (let i = 1; i <= 50; i++) {
+//     if (i % 2 != 0) {
+//         console.log(i);
+//     }
+// }
+
+// =================================Strings===================================//
+
+// let str = "bca project";
+// console.log(str.length);
+
+// ========================String method============================//
+
+// ---------Uppercase-----------//
+
+// {
+//     let str = "bcaproject";
+//     let uppr = str.toUpperCase();
+//     console.log(uppr);
+// }
+
+// ----------lowercase----------//
+
+// {
+//     let str = "bcaproject";
+//     let lower = str.toLowerCase();
+//     console.log(lower);
+// }
+
+// // ----------trim-------------//
+
+// {
+//     let str = "bca project 2025";
+//     let trim = str.trim();
+//     console.log(trim);
+// }
+
+
+// --------slice---------//
+// {
+//     let str = "0123456789";
+//     let slice = str.slice(2, 6);
+//     console.log(slice);
+// }
+
+// ------------concat----------//
+
+// {
+//     let str1 = "bca";
+//     let str2 = "project";
+//     let concat = str1.concat(str2);
+//     console.log(concat);
+// }
+
+// // ----------replace----------//
+// {
+//     let str = "bca project";
+//     let replace = str.replace("bca", "BCA");
+//     console.log(replace);
+// }
+
+// -----------charAt------------//
+
+// {
+//     let str = "bca project";
+//     let charAt = str.charAt(2);
+//     console.log(charAt);
+// }
