cc93b7984b3d09851cc745e502f7eeac105d7050
Manjunath <manjumc071@gmail.com>
2025-05-25 11:38:45 +0530

    file added

--- js/array.js
+++ js/array.js
@@ -128,6 +128,12 @@
 // marks.pop();
 // console.log(marks);
 
+// --------------- delete value -------------- //
+
+// let marks=[1,2,3,4,5];
+// let deleteValue=marks.pop();
+// console.log("deleted Value",deleteValue);
+
 // -----------concatination of two Arrays ---------------//
 
 // const roll=[1,2,3,4,5];
@@ -135,78 +141,35 @@
 // const rollnumbers=roll.concat(numbers);
 // console.log("merged Array:",rollnumbers);
 
+// ---------------- concatination of multiple arrays ---------------- //
+// const arr1=["B","C","A"];
+// const arr2=["D","E","F"];
+// const arr3=["G","H","I"];
+// const names=arr1.concat(arr2,arr3);
+// console.log(names);
+
+// ---------------using join method it converts the array into string--------------- //
+// const arr1=["B","C","A"];
+// const arr2=["D","E","F"];
+// const arr3=["G","H","I"];
+// const names=arr1.concat(arr2,arr3);
+// console.log(names.join(""));
+
 // ----------sorting the array---------- //
 
 // const numbers=[3,4,2,6,8,1,5,7];
 // console.log(numbers.sort());
+
+// ----------sorting the string---------- //
+
+// const names=["B","C","A","D","E","F"];
+// console.log(names.sort());
 
 // ----------reverse the array---------- //
 
 // const numbers=[8,7,6,5,4,3,2,1];
 // console.log(numbers.reverse());
 
-// =============Practice=============== //
-
-// function add(num1,num2){
-//     return num1+num2;
-// }
-// function sub(num1,num2){
-//     return num1-num2;
-// }
-// function mul(num1,num2){
-//     return num1*num2;
-// }
-// function div(num1,num2){
-//     return num1/num2;
-// }
-// console.log(add(10,20));
-// console.log(sub(10,20));
-// console.log(mul(10,20));
-// console.log(div(10,20));
-
-// ------------local variable---------- //
-// function myFunction(){
-//     let a=4;
-//     return a*a;
-// }
-// console.log(myFunction());
-
-// --------global variable-------- //
-// let a=4;
-// function myFunction(){
-//     return a*a;
-// }
-// console.log(myFunction());
-
-
-// ------------callback---------- //
-
-// function myDisplayer(message)
-// {
-// console.log(message);
-// }
-// function myFirst()
-// {
-// myDisplayer("I will call back later!");
-// }
-// myFirst();
-
-// ----------"hello, goodbye!"----------- //
-
-// function myDisplayer(message)
-// {
-//     console.log(message);
-// }
-
-// function myFirst()
-// {
-//     myDisplayer("hello");
-// }
-
-// function mySecond()
-// {
-//     myDisplayer("Goodbye");
-// }
-
-// myFirst();
-// mySecond();
+// --------------- reverse the string ---------------- //
+// const names=["B","C","A","D","E","F"];
+// console.log(names.reverse());
---
+++ js/obj_&_fun.js
@@ -0,0 +1,65 @@
+// =============Practice=============== //
+
+// function add(num1,num2){
+//     return num1+num2;
+// }
+// function sub(num1,num2){
+//     return num1-num2;
+// }
+// function mul(num1,num2){
+//     return num1*num2;
+// }
+// function div(num1,num2){
+//     return num1/num2;
+// }
+// console.log(add(10,20));
+// console.log(sub(10,20));
+// console.log(mul(10,20));
+// console.log(div(10,20));
+
+// ------------local variable---------- //
+// function myFunction(){
+//     let a=4;
+//     return a*a;
+// }
+// console.log(myFunction());
+
+// --------global variable-------- //
+// let a=4;
+// function myFunction(){
+//     return a*a;
+// }
+// console.log(myFunction());
+
+
+// ------------callback---------- //
+
+// function myDisplayer(message)
+// {
+// console.log(message);
+// }
+// function myFirst()
+// {
+// myDisplayer("I will call back later!");
+// }
+// myFirst();
+
+// ----------"hello, goodbye!"----------- //
+
+// function myDisplayer(message)
+// {
+//     console.log(message);
+// }
+
+// function myFirst()
+// {
+//     myDisplayer("hello");
+// }
+
+// function mySecond()
+// {
+//     myDisplayer("Goodbye");
+// }
+
+// myFirst();
+// mySecond();
